7b5e4ccb3021818d1050616eda0ee1e8d287b1e7
Junior Safera <juniorsafera.dev@gmail.com>
2022-05-06 17:55:18 -0300

    Estruturando Projeto

---
+++ delivery_app_v2/lib/core/data/category_data.dart
@@ -0,0 +1,20 @@
+import 'package:delivery_app_v2/core/models/category.dart';
+
+const List<CategoryModel> categoryData = [
+  CategoryModel(
+    id: 'c1',
+    title: 'Hamburguers',
+  ),
+  CategoryModel(
+    id: 'c2',
+    title: 'Combos',
+  ),
+  CategoryModel(
+    id: 'c3',
+    title: 'Bebidas',
+  ),
+  CategoryModel(
+    id: 'c4',
+    title: 'Hamburguers',
+  ),
+];
