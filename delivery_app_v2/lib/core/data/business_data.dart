dfad5167dacb778efbf78c6ecf42aa70b71d3c5c
Junior Safera <juniorsafera.dev@gmail.com>
2022-05-09 13:44:46 -0300

    Create business_data.dart

---
+++ delivery_app_v2/lib/core/data/business_data.dart
@@ -0,0 +1,7 @@
+import 'package:delivery_app_v2/core/models/business_model.dart';
+
+const BusinessModel businessData = BusinessModel(
+  id: '001',
+  name: 'Teste Lanches',
+  available: true,
+);
