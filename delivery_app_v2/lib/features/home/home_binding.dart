26574567d402fb6d96c7807d5eecbb9446885b0b
Junior Safera <juniorsafera.dev@gmail.com>
2022-05-17 18:39:47 -0300

    Create home_binding.dart

---
+++ delivery_app_v2/lib/features/home/home_binding.dart
@@ -0,0 +1,9 @@
+import 'package:delivery_app_v2/features/home/home_controller.dart';
+import 'package:get/get.dart';
+
+class HomeBinding implements Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<HomeController>(() => HomeController());
+  }
+}
