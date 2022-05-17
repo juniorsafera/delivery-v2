030d8a2e79bedcad706e05b7ec3fc01a587df1e7
Junior Safera <juniorsafera.dev@gmail.com>
2022-05-17 18:41:06 -0300

    Update business_model.dart

--- delivery_app_v2/lib/core/models/business_model.dart
+++ delivery_app_v2/lib/core/models/business_model.dart
@@ -1,13 +1,19 @@
 class BusinessModel {
   final String id;
   final String name;
-  final String? imageUrl;
   final bool available;
+  final String? imageUrl;
+  final String? instagramUrl;
+  final String? facebookUrl;
+  final String? whatsappUrl;
 
   const BusinessModel({
     required this.id,
     required this.name,
-      this.imageUrl,
     required this.available,
+      this.imageUrl,
+      this.instagramUrl,
+      this.facebookUrl,
+      this.whatsappUrl,
   });
 }
