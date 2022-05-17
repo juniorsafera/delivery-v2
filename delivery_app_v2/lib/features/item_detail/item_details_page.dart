742797f7b7bf7a6da1c0734e5af0df3fb329db05
Junior Safera <juniorsafera.dev@gmail.com>
2022-05-17 18:38:26 -0300

    Create item_details_page.dart

---
+++ delivery_app_v2/lib/features/item_detail/item_details_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ItemDetailsPage extends StatelessWidget {
+  const ItemDetailsPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
