8653089a990eb3a57c56f4482a24facb35b23f24
Raphael Pontes <juniorsafera.dev@gmail.com>
2022-05-19 18:25:19 -0300

    Update routes.dart

--- delivery_app_v2/lib/core/routes/routes.dart
+++ delivery_app_v2/lib/core/routes/routes.dart
@@ -1,6 +1,7 @@
 
 
 class Routes{
+ static const splahScreen = "/splash-screen";
  static const homePage = "/home-page";
  static const cartPage = "/cart-page";
  static const itemDetailsPage = "/item-details-page";
