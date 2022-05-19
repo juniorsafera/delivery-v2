ff78d71cc2462324daef815d4b243e1766e97643
Raphael Pontes <juniorsafera.dev@gmail.com>
2022-05-19 18:24:30 -0300

    Update item_details_binding.dart

--- delivery_app_v2/lib/features/item_detail/item_details_binding.dart
+++ delivery_app_v2/lib/features/item_detail/item_details_binding.dart
@@ -1,4 +1,5 @@
-import 'package:delivery_app_v2/features/item_detail/item_details.controller.dart';
+
+import 'package:delivery_app_v2/features/item_detail/item_details_controller.dart';
 import 'package:get/get.dart';
 
 class ItemDetailsBinding implements Bindings {
