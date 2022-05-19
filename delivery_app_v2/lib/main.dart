fe71626aeedaa0ec3eda48792e5751eed6556521
Raphael Pontes <juniorsafera.dev@gmail.com>
2022-05-19 18:23:27 -0300

    Update main.dart

--- delivery_app_v2/lib/main.dart
+++ delivery_app_v2/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:delivery_app_v2/core/routes/pages.dart';
+import 'package:delivery_app_v2/core/routes/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() {
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
-    initialRoute: "/",
+    initialRoute: Routes.splahScreen,
     getPages: AppPages.pages,
   ));
 }
