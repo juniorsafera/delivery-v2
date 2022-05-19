7701ce1c5d64ad665ab3c2671a431ba1ca39aca7
Raphael Pontes <juniorsafera.dev@gmail.com>
2022-05-19 09:17:16 -0300

    Update item_details.controller.dart

--- delivery_app_v2/lib/features/item_detail/item_details.controller.dart
+++ delivery_app_v2/lib/features/item_detail/item_details.controller.dart
@@ -1,9 +1,27 @@
 import 'package:delivery_app_v2/core/models/item_model.dart';
 import 'package:get/get.dart';
 
+import '../../core/routes/routes.dart';
+
 class ItemDetailsController extends GetxController {
-  ItemModel arg = Get.arguments;
+  RxList<ItemModel> arg = [
+    const ItemModel(
+        id: '0',
+        title: 'title',
+        description: 'description',
+        categories: [],
+        available: false,
+        price: 0)
+  ].obs;
 
-
-  
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+    if (Get.arguments != null) {
+      arg.value = [Get.arguments];
+    } else {
+      Get.offNamed(Routes.homePage);
+    }
+  }
 }
